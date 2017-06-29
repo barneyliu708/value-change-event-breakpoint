d95313d0a2041d1fc5698396c6727471c3e0f52b
barneyliu708 <barneyliu708@gmail.com>
2017-06-29 15:17:49 -0400

    initial commit

---
+++ ValueChangeEventBreakpoint.js
@@ -0,0 +1,40 @@
+//define a kind of watcher to innvoke an event when a property value of the object is changed 
+
+//debug: object change event start
+console = console || {}; // just in case
+console.watch = function (oObj, sProp) {
+    sPrivateProp = "$_" + sProp + "_$"; // to minimize the name clash risk
+    oObj[sPrivateProp] = oObj[sProp];
+
+    // overwrite with accessor
+    Object.defineProperty(oObj, sProp, {
+        get: function () {
+            return oObj[sPrivateProp];
+        },
+
+        set: function (value) {
+		    oObj[sPrivateProp] = value;
+			if(value === null){
+			   console.log("setting " + sProp + " to " + null); 
+			}
+			else if(value === undefined){
+			   console.log("setting " + sProp + " to " + undefined); 
+			}
+			else if(value === ""){
+			   console.log("setting " + sProp + " to " + "\"\""); 
+			}
+			else{
+			   console.log("setting " + sProp + " to " + value); 
+			}          
+			//console.trace(); //log the trace
+            debugger; // sets breakpoint
+            
+        }
+    });
+}
+//debug: object change event end
+
+//invocation
+console.watch(obj, "someProp");
+
+ allBeneficiaryAccDetails[accountNumber]
